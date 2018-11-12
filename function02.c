1c6b8f0ca0deb85f99308c661398904d589dc49d
谢明 <ming.xie@socialcredits.cn>
2018-11-12 15:32:28 +0800

    feat: 回调函数\排序

---
+++ function02.c
@@ -0,0 +1,18 @@
+//
+// Created by xm on 18-11-1.
+//
+
+#include <stdio.h>
+
+int max(int *a, int *b);
+
+void main() {
+    int a = 45, b = 56;
+    int (*fun)();
+    fun = max;
+    printf("%d ", (*fun)(&a, &b));
+    printf("%.12f", 0.1+0.2);
+}
+int max(int *a, int *b) {
+    return *a > *b ? *a : *b;
+}
--- sort.c
+++ sort.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-void swap(int *arr, int a, int b, int (*callBack)());
-int sum(int *a, int *b);
+void swap(int *arr, int a, int b);
 
 void main() {
-    int array[10] = {30, 70, 2, 36, 65, 45, -3, 32, 68, 38};
+    int array[9] = {30, 70, 2, 36, 65, 45, -3, 32, 68};
     int length = sizeof(array) / sizeof(int);
-    int (*callBack)();
-    callBack = sum;
     for (int index = 0; index < length; ++index) {
         for (int col = 0; col < length - index -1; ++col) {
-            swap(array, col, col+1, callBack);
+            swap(array, col, col+1);
         }
     }
 
@@ -19,17 +16,11 @@ void main() {
     }
 }
 
-int sum(int *a, int *b) {
-    return *a + *b;
-}
-
-void swap(int *arr, int a, int b, int (*callBack)()) {
+void swap(int *arr, int a, int b) {
     int temp;
     if (arr[a] > arr[b]) {
         temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
-
-    printf("Sum: %d + %d = %d \n", arr[a], arr[b], (*callBack)(&arr[a], &arr[b]) );
 }
